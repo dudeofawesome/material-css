c97d3294b5b1952cc5d1e83ab45c5699449fc540
Louis Orleans <louis@orleans.io>
2017-07-02 22:29:12 -0700

    🚧✨🚨🐛 Adding linting and fixing bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,28 @@
-'use strict';
-
 const gulp = require('gulp');
 
 let sass;
 gulp.task('build:sass:demo', () => {
-    if (!sass) {
-        sass = require('gulp-sass');
-    }
-    gulp.src(['demo/css/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('demo/css'));
+  if (!sass) {
+    sass = require('gulp-sass');
+  }
+  gulp.src(['demo/css/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('demo/css'));
+});
+
+let stylelint;
+gulp.task('lint:sass', () => {
+  if (!stylelint) {
+    stylelint = require('gulp-stylelint');
+  }
+  gulp.src(['*.scss'])
+    .pipe(stylelint({reporters: [{formatter: 'string', console: true}]}));
 });
 
 gulp.task('watch:demo', () => {
-    gulp.watch(['*.scss'], ['build:sass:demo']);
+  gulp.watch(['*.scss'], ['build:sass:demo']);
 });
 
+gulp.task('lint', ['lint:sass']);
 gulp.task('dev', ['watch:demo']);
-
 gulp.task('default', ['build:sass:demo']);
